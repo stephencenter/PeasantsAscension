023890cbccecff7a940f64437573d4c11b9cfa76
Stephen Center <ninjafurret@gmail.com>
2015-03-25 20:18:46 -0400

    The titlescreen has been modified to have a nicer-looking logo and be larger horizontally. Copyright (or copyleft, in this case) information is now linked to on the titlescreen, as well as a link to the download page of the game!

--- Game Files/PythoniusRPG.py
+++ Game Files/PythoniusRPG.py
@@ -845,15 +845,17 @@ def title_screen():
     pygame.mixer.music.set_volume(music_vol)
 
     print("""\
-      ____        _   _                 _
-     |  _ \\ _   _| |_| |__   ___  _ __ (_)_   _ ___
-     | |_) | | | | __| '_ \\ / _ \\| '_ \\| | | | / __|
-     |  __/| |_| | |_| | | | (_) | | | | | |_| \\__ \\
-     |_|    \\__, |\\__|_| |_|\\___/|_| |_|_|\\__,_|___/
-            |___/
-
-PythoniusRPG {0} -- Programmed in Python by Stephen Center
------------------------------------------------------------""".format(game_version))
+ ____          _    _                    _              ____   ___     ____
+|  _ \  _   _ | |_ | |__    ___   _ __  (_) _   _  ___ |  _ \ |  _ \  / ___|
+| |_) || | | || __|| '_ \  / _ \ | '_ \ | || | | |/ __|| |_) || |_) || |  _
+|  __/ | |_| || |_ | | | || (_) || | | || || |_| |\__ \|  _ < |  __/ | |_| |
+|_|     \__, | \__||_| |_| \___/ |_| |_||_| \__,_||___/|_| \_\|_|     \____|
+        |___/
+PythoniusRPG {0} -- Programmed in Python by Stephen Center (RbwNjaFurret)
+Licensed under the GNU GPLv3: [https://www.gnu.org/copyleft/gpl.html]
+Check [http://www.rbwnjafurret.com/pythoniusrpg/] periodically for updates!
+------------------------------------------------------------------------------""".format(
+        game_version))
 
     while True:
         choice = input('[P]lay Game | [C]redits | [S]tory | [E]xit  |  Input Letter: ')
