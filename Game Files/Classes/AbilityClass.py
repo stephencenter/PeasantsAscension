488b337bc81b72c09c00320d967a5336db00ecfc
TheFrozenMawile <ninjafurret@gmail.com>
2017-02-19 04:01:52 -0500

    Added the Ultimate for the Paladin Class, "Canonize". Reworded several ability descriptions, and completely reworked the design for Skill Shot. The new one is a lot more useful and cool.

--- Game Files/Classes/AbilityClass.py
+++ Game Files/Classes/AbilityClass.py
@@ -16,10 +16,11 @@ else:
 # Abilites are similar to magic, except they are tied to classes and are level-independent. They also
 # cost Action Points to use instead of Mana, and tend to have much more specific functions.
 class Ability:
-    def __init__(self, name, desc, ap_cost):
+    def __init__(self, name, desc, ap_cost, duration=0):
         self.name = name
         self.desc = desc
         self.ap_cost = ap_cost
+        self.duration = 0
 
     def use_ability(self, user):
         pass
@@ -28,7 +29,7 @@ class Ability:
         pass
 
 
-# 13 of 24 designed
+# 14 of 24 designed
 # 3 of 24 implemented
 
 # -- WARRIOR ABILITES, scales with Strength -- #
@@ -40,8 +41,9 @@ def after_parry(user):
     pass
 
 
-parry = Ability("Parry", """The user will move last this turn. If they are attacked, they will
-take no damage and will reflect [50 + Strength]% of the damage to the attacker.""", 5)
+parry = Ability("Parry", """\
+The user will move last this turn. If they are attacked, they will take no
+damage and will reflect [50 + Strength]% of the damage to the attacker.""", 5)
 parry.use_ability = use_parry
 parry.after_turn = after_parry
 
@@ -54,8 +56,9 @@ def after_roll_call(user):
     pass
 
 
-roll_call = Ability("Roll Call", """Each of the user's allies have their physical defense increased by "
-[(5 + Strength) x Number of allies]""", 5)
+roll_call = Ability("Roll Call", """\
+The user rally's their allies to fight, causing the physical defense of each
+one to increase by [(5 + Strength) x Number of allies]""", 5)
 roll_call.use_ability = use_roll_call
 roll_call.after_turn = after_roll_call
 
@@ -85,7 +88,8 @@ def after_chakra_smash(user):
     pass
 
 
-chakra_smash = Ability("Chakra Smash", """Deals a 2.5x critical strike to the enemy, lowering their defensive stats
+chakra_smash = Ability("Chakra Smash", """\
+Deals a 2.5x critical strike to the enemy, lowering their defensive stats
 by [5 + Constitution]. The armor reduction lasts indefinitely and stacks
 with multiple uses.""", 5)
 chakra_smash.use_ability = use_chakra_smash
@@ -101,7 +105,8 @@ def after_inject_poison(user):
     pass
 
 
-inject_poison = Ability("Inject Poison", """Injects a poison into the enemy target that deals [2 + Dexterity] magical
+inject_poison = Ability("Inject Poison", """\
+Injects a poison into the enemy target that deals [2 + Dexterity] magical
 damage per turn. Stacks with multiple uses, with each stack increasing damage
 dealt per turn by 2.""", 5)
 inject_poison.use_ability = use_inject_poison
@@ -116,8 +121,9 @@ def after_backstab(user):
     pass
 
 
-backstab = Ability("Backstab", """The user sneaks up on their opponent and deals a [125 + Dexterity]% critical strike.
-This move has increased priority and will always go first.""", 2)
+backstab = Ability("Backstab", """\
+The user sneaks up on their opponent and deals a [125 + Dexterity]% critical
+strike. This move has increased priority and will always go first.""", 2)
 backstab.use_ability = use_backstab
 backstab.after_turn = after_backstab
 
@@ -131,8 +137,10 @@ def after_skill_shot(user):
     pass
 
 
-skill_shot = Ability("Skill Shot", """Deals damage to the target equal to [User's Level + Target's level].
-If the user's level is lower than the enemy's level, the user's mana is restored by [5 + Intelligence].""", 2)
+skill_shot = Ability("Skill Shot", """\
+The user launches a splash-damage attack at the enemy team equal to the sum of
+their levels. If the user is higher level than the highest-levelled opponenet,
+Skill Shot does [50 + Intelligence]% more damage.""", 2)
 skill_shot.use_ability = use_skill_shot
 skill_shot.after_turn = after_skill_shot
 
@@ -145,7 +153,8 @@ def after_polymorph(user):
     pass
 
 
-polymorph = Ability("Polymorph", """Turns the enemy unit into a frog for one turn, reducing their attack
+polymorph = Ability("Polymorph", """\
+Turns the enemy unit into a harmless frog for one turn, reducing their attack
 stats, speed, and evasion to 0. The user's magic attack is also increased by
 [5 + Intelligence]. Stacks with multiple uses.""", 5)
 polymorph.use_ability = use_polymorph
@@ -160,8 +169,9 @@ def after_spell_shield(user):
     pass
 
 
-spell_shield = Ability("Spell Shield", """Places a protective barrier around your party that lowers incoming
-magical damage by [20 + Intelligence]% for 5 turns.""", 5)
+spell_shield = Ability("Spell Shield", """\
+Places a protective barrier around your party that lowers incoming magical
+damage by [20 + Intelligence]% for 5 turns.""", 5)
 spell_shield.use_ability = use_spell_shield
 spell_shield.after_turn = after_spell_shield
 
@@ -175,9 +185,10 @@ def after_roll(user):
     pass
 
 
-roll = Ability("Roll", """The user does a quick tuck-and-roll, disorienting the enemy and increasing the user's evasion
-to 256 for one turn. Also increases their speed by [25 + Perception]. Stacks
-with multiple uses.""", 3)
+roll = Ability("Roll", """\
+The user does a quick tuck-and-roll, disorienting the enemy and increasing
+the user's evasion to 256 for one turn. Also increases their speed by
+[25 + Perception]. Stacks with multiple uses.""", 3)
 roll.use_ability = use_roll
 roll.after_Turn = after_roll
 
@@ -207,8 +218,9 @@ def after_scout(user):
     pass
 
 
-scout = Ability("Scout", """Scouts the enemy, revealing their stats and elemental weakness. Also increases Pierce
-attack by [5 + Perception]%. Stacks with multiple uses.""", 1)
+scout = Ability("Scout", """\
+Scouts the enemy, revealing their stats and elemental weakness. Also increases
+Pierce attack by [5 + Perception]%. Stacks with multiple uses.""", 1)
 scout.use_ability = use_scout
 scout.after_turn = after_scout
 
@@ -222,8 +234,9 @@ def after_tip_the_scales(user):
     pass
 
 
-tip_the_scales = Ability("Tip the Scales", """The user and their allies are healed for [15 + Wisdom] HP each, while
-dealing the same in magical damage to the enemy.""", 5)
+tip_the_scales = Ability("Tip the Scales", """\
+The user and their allies are healed for [15 + Wisdom] HP each, while dealing
+the same in magical damage to the enemy.""", 5)
 tip_the_scales.use_ability = use_tip_the_scales
 tip_the_scales.after_turn = after_tip_the_scales
 
@@ -237,8 +250,9 @@ def after_unholy_binds(user):
     pass
 
 
-unholy_binds = Ability("Unholy Binds", """Sets the enemy's element to Darkness, causing your Light spells to
-do more damage. Also restores [5 + Wisdom] mana.""", 5)
+unholy_binds = Ability("Unholy Binds", """\
+Sets the enemy's element to Darkness, causing Light spells to deal more
+damage to it. Also restores [5 + Wisdom] mana.""", 5)
 unholy_binds.use_ability = use_unholy_binds
 unholy_binds.after_turn = after_unholy_binds
 
@@ -251,13 +265,32 @@ def after_judgement(user):
     pass
 
 
-judgement = Ability("Judgement", """Applies DOOM to the target, guaranteeing their death in 7 turns. If the target
-dies before the 7 turns is up, the user restores [5 + Wisdom] HP.""", 5)
+judgement = Ability("Judgement", """\
+Applies a DOOM to the target, guaranteeing their death in 7 turns. If the target
+dies before the 7 turns is up, the one who dealt the killing blow restores
+[5 + Wisdom] HP.""", 5)
 judgement.use_ability = use_judgement
 judgement.after_turn = after_judgement
 
+
+def use_canonize(user):
+    pass
+
+
+def after_canonize(user):
+    pass
+
+
+canonize = Ability("Canonize", """\
+Declares the target ally a holy figure, rendering them immune to physical
+damage, converting their element to Light, and causing all offensive light
+and dark magic casted on them to heal for [50 + Wisdom]% HP instead of
+damaging. Lasts 2 turns.""", 5)
+canonize.use_ability = use_canonize
+canonize.after_turn = after_canonize
+
 class_abilities = {
-    'paladin': [tip_the_scales, unholy_binds, judgement],
+    'paladin': [tip_the_scales, unholy_binds, judgement, canonize],
     'mage': [skill_shot, polymorph, spell_shield],
     'warrior': [roll_call, parry],
     'assassin': [inject_poison, backstab],
