5e79e7c97cc2653714ab6739b692829c6644a4c8
TheFrozenMawile <ninjafurret@gmail.com>
2017-02-19 02:32:17 -0500

    Added a blank "after_turn()" method to each of the class abilities. Also reworded the Backstab ability to be less vague.

--- Game Files/Classes/AbilityClass.py
+++ Game Files/Classes/AbilityClass.py
@@ -35,18 +35,29 @@ class Ability:
 def use_parry(user):
     pass
 
+
+def after_parry(user):
+    pass
+
+
 parry = Ability("Parry", """The user will move last this turn. If they are attacked, they will
 take no damage and will reflect [50 + Strength]% of the damage to the attacker.""", 5)
 parry.use_ability = use_parry
+parry.after_turn = after_parry
 
 
 def use_roll_call(user):
     pass
 
 
+def after_roll_call(user):
+    pass
+
+
 roll_call = Ability("Roll Call", """Each of the user's allies have their physical defense increased by "
 [(5 + Strength) x Number of allies]""", 5)
 roll_call.use_ability = use_roll_call
+roll_call.after_turn = after_roll_call
 
 
 # -- MONK ABILITIES, scales with Constitution -- #
@@ -70,10 +81,15 @@ def use_chakra_smash(user):
     return True
 
 
+def after_chakra_smash(user):
+    pass
+
+
 chakra_smash = Ability("Chakra Smash", """Deals a 2.5x critical strike to the enemy, lowering their defensive stats
 by [5 + Constitution]. The armor reduction lasts indefinitely and stacks
 with multiple uses.""", 5)
 chakra_smash.use_ability = use_chakra_smash
+chakra_smash.after_turn = after_chakra_smash
 
 
 # -- ASSASSIN ABILITIES, scales with Dexterity -- #
@@ -81,18 +97,29 @@ def use_inject_poison(user):
     pass
 
 
+def after_inject_poison(user):
+    pass
+
+
 inject_poison = Ability("Inject Poison", """Injects a poison into the enemy target that deals [2 + Dexterity] magical
 damage per turn. Stacks with multiple uses, with each stack increasing damage
 dealt per turn by 2.""", 5)
 inject_poison.use_ability = use_inject_poison
+inject_poison.after_turn = after_inject_poison
 
 
 def use_backstab(user):
     pass
 
 
-backstab = Ability("Backstab", """The user will move first and deal a [125 + Dexterity]% critical strike.""", 2)
+def after_backstab(user):
+    pass
+
+
+backstab = Ability("Backstab", """The user sneaks up on their opponent and deals a [125 + Dexterity]% critical strike.
+This move has increased priority and will always go first.""", 2)
 backstab.use_ability = use_backstab
+backstab.after_turn = after_backstab
 
 
 # -- MAGE ABILITIES, scales with Intelligence -- #
@@ -100,12 +127,21 @@ def use_skill_shot(user):
     pass
 
 
+def after_skill_shot(user):
+    pass
+
+
 skill_shot = Ability("Skill Shot", """Deals damage to the target equal to [User's Level + Target's level].
 If the user's level is lower than the enemy's level, the user's mana is restored by [5 + Intelligence].""", 2)
 skill_shot.use_ability = use_skill_shot
+skill_shot.after_turn = after_skill_shot
 
 
 def use_polymorph(user):
+    pass
+
+
+def after_polymorph(user):
     pass
 
 
@@ -113,15 +149,21 @@ polymorph = Ability("Polymorph", """Turns the enemy unit into a frog for one tur
 stats, speed, and evasion to 0. The user's magic attack is also increased by
 [5 + Intelligence]. Stacks with multiple uses.""", 5)
 polymorph.use_ability = use_polymorph
+polymorph.after_turn = after_polymorph
 
 
 def use_spell_shield(user):
     pass
 
 
+def after_spell_shield(user):
+    pass
+
+
 spell_shield = Ability("Spell Shield", """Places a protective barrier around your party that lowers incoming
 magical damage by [20 + Intelligence]% for 5 turns.""", 5)
 spell_shield.use_ability = use_spell_shield
+spell_shield.after_turn = after_spell_shield
 
 
 # -- RANGER ABILITIES, scales with Perception -- #
@@ -129,10 +171,15 @@ def use_roll(user):
     pass
 
 
+def after_roll(user):
+    pass
+
+
 roll = Ability("Roll", """The user does a quick tuck-and-roll, disorienting the enemy and increasing the user's evasion
 to 256 for one turn. Also increases their speed by [25 + Perception]. Stacks
 with multiple uses.""", 3)
 roll.use_ability = use_roll
+roll.after_Turn = after_roll
 
 
 def use_scout(user):
@@ -156,9 +203,14 @@ Element: {units.monster.element.title()} | Elemental Weakness: {monster_weakness
     battle.temp_stats[user.name]['p_attack'] += (5 + user.attributes['per'])
 
 
+def after_scout(user):
+    pass
+
+
 scout = Ability("Scout", """Scouts the enemy, revealing their stats and elemental weakness. Also increases Pierce
 attack by [5 + Perception]%. Stacks with multiple uses.""", 1)
 scout.use_ability = use_scout
+scout.after_turn = after_scout
 
 
 # -- PALADIN ABILITIES, scales with Wisdom -- #
@@ -166,9 +218,14 @@ def use_tip_the_scales(user):
     pass
 
 
+def after_tip_the_scales(user):
+    pass
+
+
 tip_the_scales = Ability("Tip the Scales", """The user and their allies are healed for [15 + Wisdom] HP each, while
 dealing the same in magical damage to the enemy.""", 5)
 tip_the_scales.use_ability = use_tip_the_scales
+tip_the_scales.after_turn = after_tip_the_scales
 
 
 def use_unholy_binds(user):
@@ -176,18 +233,28 @@ def use_unholy_binds(user):
     user.mana += (5 + user.attributes['wis'])
 
 
+def after_unholy_binds(user):
+    pass
+
+
 unholy_binds = Ability("Unholy Binds", """Sets the enemy's element to Darkness, causing your Light spells to
 do more damage. Also restores [5 + Wisdom] mana.""", 5)
 unholy_binds.use_ability = use_unholy_binds
+unholy_binds.after_turn = after_unholy_binds
 
 
 def use_judgement(user):
     pass
 
 
+def after_judgement(user):
+    pass
+
+
 judgement = Ability("Judgement", """Applies DOOM to the target, guaranteeing their death in 7 turns. If the target
 dies before the 7 turns is up, the user restores [5 + Wisdom] HP.""", 5)
 judgement.use_ability = use_judgement
+judgement.after_turn = after_judgement
 
 class_abilities = {
     'paladin': [tip_the_scales, unholy_binds, judgement],
@@ -199,5 +266,5 @@ class_abilities = {
 
 }
 
-print(sum(len(x) for x in class_abilities.values()))
+# print(sum(len(x) for x in class_abilities.values()))
 
